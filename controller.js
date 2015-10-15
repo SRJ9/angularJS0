f6be5d400d63088050b85bc46f94ae98fd6d7735
Jose <jose@alsur.es>
2015-10-15 13:36:54 +0200

    feat: two ways data binding, function in controller and new directives (ng-model, ng-repeat, ng-click)

--- controller.js
+++ controller.js
@@ -1,4 +1,21 @@
+// modulos:
+// * ngResource - API
 var app = angular.module("MyFirstApp", []);
 app.controller("FirstController", function($scope){
 	$scope.nombre = "Pepito";
+	$scope.nuevoComentario = {};
+	$scope.comentarios = [
+		{ 
+			comentario: "Comentario 1",
+			username: "user uno"
+		},
+		{
+			comentario: "Comentario 2",
+			username: "user two"
+		}
+	];
+	$scope.agregarComentario = function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario = {};
+	}
 });
