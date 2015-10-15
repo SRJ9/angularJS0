37b4e9dad7c5a3f7d56691ab04016712dd60f217
Jose <jose@alsur.es>
2015-10-15 14:11:56 +0200

    feat: method http:post

--- http.js
+++ http.js
@@ -3,6 +3,7 @@
 var app = angular.module("MyFirstApp", []);
 app.controller("FirstController", function($scope, $http){
 	$scope.posts = [];
+	$scope.newPost = {};
 	$http.get("http://jsonplaceholder.typicode.com/posts")
 		.success(function(data){
 			console.log(data);
@@ -10,4 +11,18 @@ app.controller("FirstController", function($scope, $http){
 		})
 		.error(function(err){
 		});
+	$scope.addPost = function(){
+		$http.post("http://jsonplaceholder.typicode.com/posts", {
+			title: $scope.newPost.title,
+			body: $scope.newPost.body,
+			userId: 1
+		})
+		.success(function(data, status, headers, config){
+			$scope.posts.push(data);
+			$scope.newPost = {};
+		})
+		.error(function(error, status, headers, config){
+			console.log(error);
+		});
+	}
 });
