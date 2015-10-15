3f865ac2510ddab716f5fa1dd1bc91d4b97cfcb2
Jose <jose@alsur.es>
2015-10-15 20:08:16 +0200

    refactor: use of factories in to-do-list

--- to-do-list.js
+++ to-do-list.js
@@ -1,28 +1,64 @@
 // modulos:
 // * ngResource - API
 var app = angular.module("ToDoList", ['LocalStorageModule']);
-app.controller("ToDoController", function($scope, localStorageService){
-	if(localStorageService.get("angular-todolist")){
-		$scope.todo = localStorageService.get("angular-todolist");
+app.factory('ToDoService', function(localStorageService){
+	var toDoService = {};
+
+	toDoService.key = 'angular-todolist';
+
+	if(localStorageService.get(toDoService.key)){
+		toDoService.activities = localStorageService.get(toDoService.key);
 	} else {
-		$scope.todo = [];
+		toDoService.activities= [];
 	}
+
+	toDoService.add = function(newActv){
+		toDoService.activities.push(newActv);
+		toDoService.updateLocalStorage();
+		// self or this ??
+	}
+
+	toDoService.updateLocalStorage = function(){
+		localStorageService.set(toDoService.key, toDoService.activities);
+	}
+
+	toDoService.clean = function(){
+		toDoService.activities = [];
+		toDoService.updateLocalStorage();
+		return toDoService.getAll();
+	}
+
+	toDoService.getAll = function(){
+		return toDoService.activities;
+	}
+
+	toDoService.removeItem = function(item){
+		toDoService.activities = toDoService.activities.filter(function(activity){
+			return activity !== item;
+		});
+		toDoService.updateLocalStorage();
+		return toDoService.getAll();
+	}
+
+	// buscar por qué watch no es recomendable
+
+	return toDoService;
+});
+app.controller("ToDoController", function($scope, ToDoService){
 	
+	$scope.todo = ToDoService.getAll();
 	$scope.newActv = {};
-	/*
-	{
-		actividad: 'Terminar curso',
-		fecha: '3-03-15 2:00pm'
-	}
-	*/
-	
-	$scope.$watchCollection('todo', function(newValue, oldValue){
-		localStorageService.set("angular-todolist", $scope.todo);
-	});
-	
 	$scope.addActv = function(){
-		$scope.todo.push($scope.newActv);
+		ToDoService.add($scope.newActv);
 		$scope.newActv = {};
 		
+	};
+
+	$scope.removeActv = function(item){
+		$scope.todo = ToDoService.removeItem(item);
+	};
+
+	$scope.clean = function(){
+		$scope.todo = ToDoService.clean();
 	}
 });
