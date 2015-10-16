ed70e32b33b0b8e90d373a7c440f4f4717df1dcb
Jose <jose@alsur.es>
2015-10-16 14:07:31 +0200

    feat: custom-directives

--- custom-directives.js
+++ custom-directives.js
@@ -1,6 +1,18 @@
 // modulos:
 // * ngResource - API
 var app = angular.module("CustomDirective", []);
+app.directive('backImg', function(){
+	return function(scope, element, attrs){
+		attrs.$observe('backImg', function(value){
+			element.css({
+				'background': 'url('+value+')',
+				'background-size': 'cover',
+				'background-position': 'center'
+			});
+		});
+
+	}
+});
 app.controller("AppCtrl", function($scope, $http){
 	$http.get('https://api.github.com/users/SRJ9/repos')
 		.success(function(data){
