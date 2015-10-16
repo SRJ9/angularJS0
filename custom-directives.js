9387c78c7287236d801ef573a6ba2d15443c83a5
Jose <jose@alsur.es>
2015-10-16 14:38:21 +0200

    feat: custom-directives, autocomplete with jquery-ui

--- custom-directives.js
+++ custom-directives.js
@@ -1,6 +1,28 @@
 // modulos:
 // * ngResource - API
 var app = angular.module("CustomDirective", []);
+app.directive('myAutocomplete', function(){
+	function link(scope, element, attrs){
+		$(element).autocomplete({
+			source: scope[attrs.myAutocomplete],
+			select: function(ev, ui){
+				ev.preventDefault();
+				if(ui.item){
+					scope.optionSelected(ui.item.value);
+				}
+
+			},
+			focus: function(ev, ui){
+				ev.preventDefault();
+				$(this).val(ui.item.label);
+			}
+		})
+
+	};
+	return {
+		link: link
+	}
+});
 app.directive('backImg', function(){
 	return function(scope, element, attrs){
 		attrs.$observe('backImg', function(value){
@@ -14,11 +36,24 @@ app.directive('backImg', function(){
 	}
 });
 app.controller("AppCtrl", function($scope, $http){
+
+	$scope.repos = [];
+
 	$http.get('https://api.github.com/users/SRJ9/repos')
 		.success(function(data){
-			$scope.repos = data;
+			$scope.posts = data;
+			for(var i=(data.length - 1); i >= 0; i-- ){
+				var repo = data[i];
+				$scope.repos.push(repo.name);
+			}
 		})
 		.error(function(err){
 			console.log(err);
-		})
+		});
+
+	$scope.optionSelected = function(data){
+		$scope.$apply(function(){
+			$scope.main_repo = data;
+		});
+	}
 });
