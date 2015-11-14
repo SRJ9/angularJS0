b8728c4ca11dbb0a14b4c6b69eedf257830fb7f0
José <jose@alsur.es>
2015-11-14 13:31:18 +0100

    feat: ng-resource. finish tutorial

--- resource/js/app.js
+++ resource/js/app.js
@@ -4,5 +4,19 @@ angular.module("FinalApp",["lumx","ngRoute", "ngResource"])
             .when("/", {
                 controller: "MainController",
                 templateUrl: "templates/home.html"
+            })
+            .when("/post/:id", {
+                controller: "PostController",
+                templateUrl: "templates/post.html"
+            })
+            .when("/posts/new", {
+                controller: "NewPostController",
+                templateUrl: "templates/post_form.html"
+
+            })
+            .when("/posts/edit/:id", {
+                controller: "PostController",
+                templateUrl: "templates/post_form.html"
+
             });
     });
